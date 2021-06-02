9384ddada1515060271bdc009f8bc847877aa7f3
Ralf Boltshauser <ra.boltshauser3@gmail.com>
2021-06-02 16:00:53 +0200

    HotFix 2

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
 import galaxyVertexShader from './shaders/galaxy/vertex.glsl';
 import galaxyFragmenShader from './shaders/galaxy/fragment.glsl';
 /**
